6d00847df03ea273621bbd33838527496dc021ce
Malachi Griffith <malachig@gmail.com>
2025-08-27 10:18:31 -0500

    update mutated fasta results

---
+++ generate_ref_peptides_mutation.sh
@@ -0,0 +1,8 @@
+python3.11 scripts/generation/generate_control_peptides.py --length 8 --source fasta --fasta_file data/refseq_protein.fa --output data/1M_Peptides/reference_8mer_1M.fasta --mutations 1 --count 1000000 --mutate
+
+python3.11 scripts/generation/generate_control_peptides.py --length 9 --source fasta --fasta_file data/refseq_protein.fa --output data/1M_Peptides/reference_9mer_1M.fasta --mutations 1 --count 1000000 --mutate
+
+python3.11 scripts/generation/generate_control_peptides.py --length 10 --source fasta --fasta_file data/refseq_protein.fa --output data/1M_Peptides/reference_10mer_1M.fasta --mutations 1 --count 1000000 --mutate
+
+python3.11 scripts/generation/generate_control_peptides.py --length 11 --source fasta --fasta_file data/refseq_protein.fa --output data/1M_Peptides/reference_11mer_1M.fasta --mutations 1 --count 1000000 --mutate
+
